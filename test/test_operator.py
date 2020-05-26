452e77cabe3264398e0aeb750bf1ebc6487d78f1
Ben Hayes <ben@benhayes.net>
2020-05-26 18:24:26 +0100

    Begin implementing operator class

---
+++ test/test_operator.py
@@ -0,0 +1,83 @@
+import unittest
+
+import torch
+
+from torch_fm_synth.operator import Operator
+
+
+class TestOperator(unittest.TestCase):
+    def test_can_create(self):
+        operator = Operator()
+        self.assertIsInstance(operator, Operator)
+
+    def test_can_synthesise_sinusoid(self):
+        dummy_sr = 2
+        dummy_freq = 1
+        sample_length = 4
+        expected_output = torch.Tensor([1, -1, 1, -1])
+
+        operator = Operator(sr=dummy_sr)
+        actual_output = operator.sample(dummy_freq, length=sample_length)
+
+        torch.testing.assert_allclose(actual_output, expected_output)
+
+    def test_can_set_amplitude(self):
+        dummy_sr = 4
+        dummy_freq = 2
+        dummy_amplitude = 0.5
+        sample_length = 4
+        expected_output = torch.Tensor([0.5, -0.5, 0.5, -0.5])
+
+        operator = Operator(sr=dummy_sr)
+        actual_output = operator.sample(
+            dummy_freq,
+            amplitude=dummy_amplitude,
+            length=sample_length)
+
+        torch.testing.assert_allclose(actual_output, expected_output)
+
+    def test_can_set_tensor_amplitude(self):
+        dummy_sr = 8
+        dummy_freq = 4
+        dummy_amplitude = torch.Tensor([0, 0.4, 0.9, 13.0])
+        sample_length = 4
+        expected_output = torch.Tensor([0.0, -0.4, 0.9, -13.0])
+
+        operator = Operator(sr=dummy_sr)
+        actual_output = operator.sample(
+            dummy_freq,
+            amplitude=dummy_amplitude,
+            length=sample_length)
+
+        torch.testing.assert_allclose(actual_output, expected_output)
+
+    def test_can_set_tensor_frequency(self):
+        dummy_sr = 8
+        dummy_freq = torch.Tensor([2, 0, 4, 2])
+        sample_length = 4
+        expected_output = torch.Tensor([1, 1, -1, 0])
+
+        operator = Operator(sr=dummy_sr)
+        actual_output = operator.sample(
+            dummy_freq,
+            length=sample_length)
+
+        torch.testing.assert_allclose(actual_output, expected_output)
+
+    def test_throws_if_scalar_freq_and_amp_with_no_length(self):
+        dummy_sr = 8
+        dummy_freq = 3
+        dummy_amp = 0.7
+
+        operator = Operator(sr=dummy_sr)
+        with self.assertRaises(ValueError):
+            operator.sample(dummy_freq, dummy_amp)
+
+    def test_throws_if_tensor_freq_and_amp_are_different_lengths(self):
+        dummy_sr = 8
+        dummy_freq = torch.Tensor([0.5, 0.1, 0.3])
+        dummy_amp = torch.Tensor([12, 0.1, 0.2, 0.5])
+
+        operator = Operator(sr=dummy_sr)
+        with self.assertRaises(ValueError):
+            operator.sample(dummy_freq, dummy_amp)
---
+++ torch_fm_synth/operator.py
@@ -0,0 +1,30 @@
+import math
+
+import torch
+
+
+class Operator:
+    def __init__(self, sr=44100):
+        self.sr = sr
+
+    def sample(self, freq, amplitude=1.0, length=None):
+        if (type(freq) is not torch.Tensor
+                and type(amplitude) is not torch.Tensor
+                and length is None):
+            raise ValueError("Can't use scalar frequency and amplitude " +
+                             "without specifying output length")
+
+        if (type(freq) is torch.Tensor
+                and type(amplitude) is torch.Tensor
+                and freq.shape[-1] != amplitude.shape[-1]):
+            raise ValueError("Amplitude and frequency tensor sizes do not " +
+                             "match in time dimension")
+
+        if type(freq) is not torch.Tensor:
+            freq = torch.ones(length) * freq
+
+        phase = freq.cumsum(dim=-1) * math.tau / self.sr
+        phase = phase - phase[0]
+
+        y = torch.cos(phase) * amplitude
+        return y
